34b1c4626cd0a34b31659bd08b7ab0c23425aa74
GHOST <3089308393@qq.com>
2025-05-04 23:59:32 +0800

    feat: 底部导航条

--- apps/user/src/app.config.js
+++ apps/user/src/app.config.js
@@ -2,7 +2,8 @@ export default defineAppConfig({
   pages: [
     'pages/index/index',
     'pages/myTravels/myTravels',
-    'pages/login/index'
+    'pages/login/index',
+    'pages/addTravel/addTravel'
   ],
   window: {
     backgroundTextStyle: 'light',
@@ -10,4 +11,30 @@ export default defineAppConfig({
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black',
   },
+  tabBar: {
+    color: '#333',
+    selectedColor: '#ea0000',
+    backgroundColor: '#fff',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+        iconPath: 'assets/unselectedHome.png',
+        selectedIconPath: 'assets/selectedHome.png'
+      },
+      {
+        pagePath: 'pages/addTravel/addTravel',
+        text: '游记发布',
+        // iconPath: 'images/icon3.png',
+        // selectedIconPath: 'images/icon3_active.png'
+      },
+      {
+        pagePath: 'pages/myTravels/myTravels',
+        text: '我的游记',
+        // iconPath: 'images/icon2.png',
+        // selectedIconPath: 'images/icon2_active.png'
+      }
+    ],
+    borderStyle: 'black'
+  }
 });

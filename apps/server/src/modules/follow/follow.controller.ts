fd0172bf259bb1521dd3da11d8d154f4b5ebd013
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-05-07 19:45:45 +0800

    feat(关注模块): 实现关注和取消关注功能

--- apps/server/src/modules/follow/follow.controller.ts
+++ apps/server/src/modules/follow/follow.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, ParseIntPipe, Post } from '@nestjs/common';
 import { FollowService } from './follow.service';
+import { JwtPayload } from '@triptrip/utils';
+import { User } from 'src/common/decorators/user.decorator';
 
 @Controller('follow')
 export class FollowController {
   constructor(private readonly followService: FollowService) {}
+
+  @Post()
+  addFollow(@User() user: JwtPayload, @Body('followerId', ParseIntPipe) followingId: number) {
+    this.followService.addFollow(user.uid, followingId);
+  }
+
+  @Delete()
+  removeFollow(@User() user: JwtPayload, @Body('followId', ParseIntPipe) followingId: number) {
+    this.followService.removeFollow(user.uid, followingId);
+  }
 }
--- apps/server/src/modules/follow/follow.service.ts
+++ apps/server/src/modules/follow/follow.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/common/utils/prisma/prisma.service';
 
 @Injectable()
-export class FollowService {}
+export class FollowService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async addFollow(followerId: number, followingId: number) {
+    await this.prismaService.userFollow.create({
+      data: {
+        followerId,
+        followingId,
+      }
+    })
+  }
+  async removeFollow(followerId: number, followingId: number) {
+    await this.prismaService.userFollow.delete({
+      where: {
+        followerId_followingId: {
+          followerId,
+          followingId,
+        }
+      }
+    })
+  }
+
+}

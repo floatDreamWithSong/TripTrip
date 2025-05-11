69feaf4875662ea153759332c4c0e2a7b784cfa5
GHOST <3089308393@qq.com>
2025-05-12 00:27:41 +0800

    feat: 从我的界面中添加按钮跳转到编辑界面（已发布的游记不可编辑）跳转时将信息放在内存，修改时调用内存数据进行修改

--- apps/user/src/components/MyCard.jsx
+++ apps/user/src/components/MyCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, Image, Text } from '@tarojs/components';
 import './MyCard.scss';
 import { Button } from 'antd';
+import Taro from '@tarojs/taro';
 
 const stateClassMap = {
   '已通过': 'approved',
@@ -14,6 +15,31 @@ const TravelCard = ({ travel }) => {
 
   const toDetail = (id) => {
     console.log('跳转到详情页', id);
+    if (travel.state === '待审核') {
+      Taro.showToast({
+        title: '请等待审核结果，或点击下方编辑按钮',
+        icon: 'none'
+      });
+    } else if (travel.state === '未通过') {
+      Taro.showToast({
+        title: '审核未通过，请查看原因并修改',
+        icon: 'none'
+      });
+    } else {
+      console.log('跳转到详情页面');
+      Taro.navigateTo({
+        url: `/pages/travelDetail/travelDetail?id=${id}`
+      });
+    }
+  };
+
+  const toEdit = (travel) => {
+    console.log('跳转到编辑页面', travel);
+    Taro.setStorageSync('editTravel', travel);
+    console.log(Taro.getStorageSync('editTravel'));
+    Taro.navigateTo({
+      url: 'pages/editTravel/editTravel'
+    });
   };
 
   return (
@@ -50,23 +76,35 @@ const TravelCard = ({ travel }) => {
         {travel.state}
       </View>
 
-      {travel.state === '未通过' && (
-        <View className="reject-section" onClick={(e) => e.stopPropagation()}>
-          <View
-            className="toggle-reason-btn"
-            onClick={(e) => {
-              e.stopPropagation();
-              setShowReason(!showReason);
-            }}
-          >
-            {showReason ? '收起原因' : '查看原因'}
-          </View>
-
-          {showReason && (
-            <View className="reject-reason">
-              {travel.rejectReason || '未提供具体原因'}
+      <View className='travel-btn-section'>
+        {travel.state === '未通过' && (
+          <View className="reject-section" onClick={(e) => e.stopPropagation()}>
+            <View
+              className="toggle-reason-btn"
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowReason(!showReason);
+              }}
+            >
+              {showReason ? '收起原因' : '查看原因'}
             </View>
-          )}
+
+
+          </View>
+        )}
+        {/* 添加编辑按钮 */}
+        {(travel.state === '待审核' || travel.state === '未通过') && (
+          <View className="edit-btn" onClick={(e) => {
+            e.stopPropagation();
+            toEdit(travel);
+          }}>
+            <View className='edit-btn-text'>编辑</View>
+          </View>
+        )}
+      </View>
+      {showReason && (
+        <View className="reject-reason">
+          {travel.rejectReason || '未提供具体原因'}
         </View>
       )}
     </View>
